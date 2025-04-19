4d1871606819a47baeb27b7a43bcd0fe57873122
piyush0189 <piyushpushkar2004@gmail.com>
2025-04-19 19:16:22 +0530

    Add Project  Files

---
+++ public/js/map.js
@@ -0,0 +1,12 @@
+    mapboxgl.accessToken = mapToken;
+
+    const map = new mapboxgl.Map({
+        container: "map", // container ID
+        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
+        zoom: 8, // starting zoom
+    });
+
+    const marker = new mapboxgl.Marker({color: "Red"})
+    .setLngLat(listing.geometry.coordinates)    //Listing.geometry.coordinates
+    .setPopup(new mapboxgl.Popup({offset: 25}).setHTML(`<h4>${listing.title}</h4><p>Exact Location will be Provided after booking</p>`))
+    .addTo(map);
